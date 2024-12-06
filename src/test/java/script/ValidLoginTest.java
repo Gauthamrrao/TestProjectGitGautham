cdaaad9b4e9de8a4111d3ef397ec79d7faa6d00b
My PC <My PC@DESKTOP-AF29E4S>
2024-12-06 13:53:07 +0530

    Taking input from the Excel sheet and updated the excel document

--- src/test/java/script/ValidLoginTest.java
+++ src/test/java/script/ValidLoginTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Excel;
 import page.EnterTimeTrackPage;
 import page.LoginPage;
 
@@ -12,11 +13,13 @@ public class ValidLoginTest extends BaseTest
 	@Test(priority=1)
 	public void testValidLogin()
 	{
+		String un=Excel.getData(XL_PATH, "ValidLogin", 1, 0);
+		String pw=Excel.getData(XL_PATH, "ValidLogin", 1, 1);
 		LoginPage loginpage=new LoginPage(driver);
 		//1.Enter valid username
-		loginpage.setUserName("admin");
+		loginpage.setUserName(un);
 		//2.Enter the valid password
-		loginpage.setPassword("manager");
+		loginpage.setPassword(pw);
 		//3.click Login Button
 		loginpage.clickLoginButton();
 		//4.verifyHomepage
